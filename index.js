83f603182984db916ffd291bff106a6fdebe6448
ivanhez <64239629+ivanhez@users.noreply.github.com>
2022-08-08 13:40:13 -0600

    crear usuario

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { client, xml } = require("@xmpp/client");
 const debug = require("@xmpp/debug");
-
 const xmpp = client({
-    service: "alumchat.fun",
-    username: "ivanh",
-    password: "123",
+  service: "alumchat.fun",
+  username: 'ivanh96',
+  password: '123',
 });
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 debug(xmpp, true);
 
@@ -13,31 +13,47 @@ xmpp.on("error", (err) => {
   console.error(err);
 });
 
-// xmpp.on("offline", () => {
-//   console.log("offline");
+xmpp.on("offline", () => {
+  console.log("offline");
+});
+
+// xmpp.on("stanza", async (stanza) => {
+//   if (stanza.is("message")) {
+//     await xmpp.send(xml("presence", { type: "unavailable" }));
+//     await xmpp.stop();
+//   }
 // });
 
-xmpp.on("stanza", async (stanza) => {
-  if (stanza.is("message")) {
-    await xmpp.send(xml("presence", { type: "unavailable" }));
-    await xmpp.stop();
-  }
-});
+// xmpp.on("online", async (address) => {
+//   // Makes itself available
+//   await xmpp.send(xml("presence"));
 
-xmpp.on("online", async (address) => {
-  // Makes itself available
-  await xmpp.send(xml("presence"));
+//   // Sends a chat message to itself
+//   const message = xml(
+//     "message",
+//     { type: "chat", to: address },
+//     xml("body", {}, "hello world")
+//   );
+//   await xmpp.send(message);
+// });
 
-  // Sends a chat message to itself
-  const message = xml(
-    "message",
-    { type: "chat", to: address },
-    xml("body", {}, "hello world"),
-  );
-  await xmpp.send(message);
-});
-
+// xmpp.on("online", async (address) => {
+//   await xmpp.send(
+//     xml(
+//       "iq",
+//       { type: "set", id: "reg2" },
+//       xml(
+//         "query",
+//         { xmlns: "jabber:iq:register" },
+//         xml("username", {}, "ivanh"),
+//         xml("password", {}, "123")
+//       )
+//     )
+//   );
+// });
+// xmpp.send(
+//     xml("iq",{ type: "get", id: "reg1", to: "alumchat.fun" },
+//       xml("query", { xmlns: "jabber:iq:register" })
+//     )
+//   );
 xmpp.start().catch(console.error);
-xmpp.on("online", (address) => {
-  console.log("online", address.toString());
-});

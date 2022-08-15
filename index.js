5598505bcae7298860a0505a3b83cd96eccce21f
ivanhez <64239629+ivanhez@users.noreply.github.com>
2022-08-14 20:25:33 -0600

    chat ok

--- index.js
+++ index.js
@@ -1,149 +1,170 @@
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const { client, xml } = require("@xmpp/client");
 const debug = require("@xmpp/debug");
-var menu = require('console-menu');
-var query = require('cli-interact').question;
+var menu = require("console-menu");
+const { stat } = require("fs");
+var query = require("cli-interact").question;
 
-menu([
-  { hotkey: '1', title: 'LOGIN' },
-  { hotkey: '2', title: 'REGISTER' },
-  // { hotkey: '3', title: 'Three' },
-  { separator: true },
-  // { hotkey: '?', title: 'Help' },
-], {
-  header: 'MENU',
-  border: true,
-}).then(item => {
-  if (item) {
-    switch (parseInt(item.hotkey)) {
-      case 1:
-        var username = query('USERNAME: ');
-        var password = query('PASSWORD: ');
-        var xmpp = client({
-          service: "alumchat.fun",
-          username: username,
-          password: password,
-        });
-        debug(xmpp, true);
-
-        xmpp.on("error", (err) => {
-          console.error(err);
-        });
-
-        xmpp.on("offline", () => {
-          console.log("offline");
-        });
-
-        xmpp.on("stanza", async (stanza) => {
-          if (stanza.is("message")) {
-            await xmpp.send(xml("presence", { type: "unavailable" }));
-            await xmpp.stop();
-          }
-        });
-
-        xmpp.on("online", async (address) => {
-          // Makes itself available
-          await xmpp.send(xml("presence"));
-
-          // Sends a chat message to itself
-          // const message = xml(
-          //   "message",
-          //   { type: "chat", to: address },
-          //   xml("body", {}, "hello world")
-          // );
-          // await xmpp.send(message);
-          cmenu(xmpp);
-        });
-        xmpp.start().catch(console.error);
-        break;
-
-      case 2:
-        var xmpp = client({
-          service: "alumchat.fun",
-          username: 'test',
-          password: 'test',
-        });
-        var username = query('NEW USERNAME: ');
-        var password = query('NEW PASSWORD: ');
-        debug(xmpp, true);
-        xmpp.on("error", (err) => {
-          console.error(err);
-        });
-        xmpp.on("online", async (address) => {
-          await xmpp.send(
-            xml(
-              "iq",
-              { type: "set", id: "reg2" },
-              xml(
-                "query",
-                { xmlns: "jabber:iq:register" },
-                xml("username", {}, username),
-                xml("password", {}, password)
-              )
-            )
-          );
-        });
-        xmpp.start().catch(console.error);
-
-        break;
-      default:
-        console.log("ERROR");
-        break;
+async function main() {
+  menu(
+    [
+      { hotkey: "1", title: "LOGIN" },
+      { hotkey: "2", title: "REGISTER" },
+      // { hotkey: '3', title: 'Three' },
+      { separator: true },
+      // { hotkey: '?', title: 'Help' },
+    ],
+    {
+      header: "MENU",
+      border: true,
     }
-  } else {
-    console.log('EXIT');
-  }
-});
-
-
-function delUser(xmpp) {
-  //   <iq type='set' id='unreg1'>
-  //      <query xmlns='jabber:iq:register'>
-  //        <remove/>
-  //      </query>
-  //   </iq>
-  xmpp.send(
-    xml(
-      "iq",
-      { type: "set", id: "unreg1" },
-      xml(
-        "query",
-        { xmlns: "jabber:iq:register" },
-        xml("remove")
-      )
-    )
-  );
-  return;
-}
-function cmenu(xmpp) {
-  menu([
-    { hotkey: '1', title: 'CHAT' },
-    { hotkey: '2', title: 'ROSTER', selected: true },
-    { hotkey: '3', title: 'EXIT' },
-    { separator: true },
-    { hotkey: '0', title: 'DELETE USER' },
-  ], {
-    header: 'NODECHAT',
-    border: true,
-  }).then(item => {
+  ).then((item) => {
     if (item) {
       switch (parseInt(item.hotkey)) {
+        case 1:
+          var username = query("USERNAME: ");
+          var password = query("PASSWORD: ");
+          var xmpp = client({
+            service: "alumchat.fun",
+            username: username,
+            password: password,
+          });
+          debug(xmpp, true);
+          xmpp.start().catch(console.error);
+          xmpp.on("error", (err) => {
+            console.error(err);
+          });
+
+          // xmpp.on("offline", () => {
+          //   console.log("offline");
+          // });
+
+          xmpp.on("stanza", async (stanza) => {
+            // if (stanza.is("message")) {
+            //   await xmpp.send(xml("presence", { type: "unavailable" }));
+            //   await xmpp.stop();
+            // }
+          });
+
+          xmpp.on("online", async (address) => {
+            await xmpp.send(xml("presence"));
+            console.log("WELCOME: " + address.user);
+            const froms = address.local + "@" + address.domain;
+            cmenu(xmpp, froms);
+          });
+          break;
+        case 2:
+          var xmpp = client({
+            service: "alumchat.fun",
+            username: "test",
+            password: "test",
+          });
+          var username = query("NEW USERNAME: ");
+          var password = query("NEW PASSWORD: ");
+          debug(xmpp, true);
+          xmpp.on("error", (err) => {
+            console.error(err);
+          });
+          xmpp.on("online", async (address) => {
+            await xmpp.send(
+              xml(
+                "iq",
+                { type: "set", id: "reg2" },
+                xml(
+                  "query",
+                  { xmlns: "jabber:iq:register" },
+                  xml("username", {}, username),
+                  xml("password", {}, password)
+                )
+              )
+            );
+          });
+          xmpp.start().catch(console.error);
+          xmpp.on("stanza", async (stanza) => {
+            if (stanza.is("iq")) {
+              if(stanza.attr.type == "result"){
+                console.log("REGISTERED NEW USER SUCCESSFULLY");
+                await xmpp.stop();
+              }
+              if(stanza.attr.type == "error"){
+                console.log("ERROR REGISTERING NEW USER");
+                await xmpp.stop();
+              }
+            }
+            return main();
+          });
+          break;
+        default:
+          console.log("ERROR");
+          break;
+      }
+    } else {
+      console.log("EXIT");
+    }
+  });
+}
+
+function cmenu(xmpp, froms) {
+  menu(
+    [
+      { hotkey: "1", title: "CHAT" },
+      { hotkey: "2", title: "ROSTER", selected: true },
+      { hotkey: "4", title: "EDIT PRESENCE" },
+      { hotkey: "3", title: "EXIT" },
+      { separator: true },
+      { hotkey: "0", title: "DELETE USER" },
+    ],
+    {
+      header: "NODECHAT",
+      border: true,
+    }
+  ).then((item) => {
+    if (item) {
+      switch (parseInt(item.hotkey)) {
+        case 1:
+          var to = query("TO: ");
+          var message = query("MESSAGE: ");
+          xmpp.send(
+            xml(
+              "message",
+              { type: "chat", to: to + "@alumchat.fun" },
+              xml("body", {}, message)
+            )
+          );
+          break;
         case 2:
           break;
         case 3:
           xmpp.disconnect();
+          main();
           break;
+        case 4:
+          // var show = query("SHOW: ");
+          var status = query("STATUS: ");
+          var priority = parseInt(query("PRIORITY: "));
+          xmpp.send(
+            xml(
+              "presence",
+              { from: froms },
+              xml("show", {}, "chat"),
+              xml("status", {}, status),
+              xml("priority", {}, priority)
+            )
+          );
+          return cmenu(xmpp, froms);
         case 0:
-          delUser(xmpp);
-          break;
+          xmpp.send(
+            xml(
+              "iq",
+              { type: "set", id: "unreg1" },
+              xml("query", { xmlns: "jabber:iq:register" }, xml("remove"))
+            )
+          );
+          return main();
       }
     } else {
       return;
     }
   });
 }
-
-
-
-
-
+main();
